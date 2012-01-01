ddce968d0e927b0ead659a7fcee1ac0a6291630e
Laide <lo00051@surrey.ac.uk>
2012-01-01 17:19:17 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SibcApp::Application.config.secret_token = 'e7f148694346d8397ae4684953247227ad0a31a774f8bcb2238e3d8d3215745e33e069291e92886b68c32c8be7bee5814652e448be69baac5bfa5541c39b4c11'
