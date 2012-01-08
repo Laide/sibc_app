8b0c335dd7a9bd69bcb9bbef84698b8256b60dd5
Laide <lo00051@surrey.ac.uk>
2012-01-08 22:54:25 +0000

    User signup complete

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -36,14 +36,74 @@ describe UsersController do
   end
 
   describe "GET 'new'" do
-    it "should be successful" do
-      get 'new'
+
+ it "should be successful" do
+      get :new
       response.should be_success
     end
 
-     it "should have the right title" do
+    it "should have the right title" do
+      get :new
+      response.should have_selector("title", :content => "Sign up")
+    end
+  it "should be successful" do
+      get 'new'
+     response.should be_success
+    end
+
+    it "should have the right title" do
        get 'new'
      response.should have_selector("title", :content => "Sign up")
-  end
  end
+ end
+describe "POST 'create'" do
+
+    describe "failure" do
+
+      before(:each) do
+        @attr = { :name => "", :email => "", :password => "",
+                  :password_confirmation => "" }
+      end
+
+      it "should not create a user" do
+        lambda do
+          post :create, :user => @attr
+        end.should_not change(User, :count)
+      end
+
+      it "should have the right title" do
+        post :create, :user => @attr
+        response.should have_selector("title", :content => "Sign up")
+      end
+
+      it "should render the 'new' page" do
+        post :create, :user => @attr
+        response.should render_template('new')
+      end
+    end
+
+  describe "success" do
+
+      before(:each) do
+        @attr = { :name => "Laide Olanlokun", :email => "laide@olanlokun.com",
+                  :password => "medlyd", :password_confirmation => "medlyd" }
+      end
+
+      it "should create a user" do
+        lambda do
+          post :create, :user => @attr
+        end.should change(User, :count).by(1)
+      end
+
+      it "should redirect to the user show page" do
+        post :create, :user => @attr
+        response.should redirect_to(user_path(assigns(:user)))
+      end
+
+  it "should have a welcome message" do
+        post :create, :user => @attr
+        flash[:success].should =~ /welcome to the sbic website /i
+      end    
+    end
+end
 end
